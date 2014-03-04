63deafa75af91b7bf91d569c37164c4d288a5055
Aires <Aires@10.16.1.232>
2014-03-04 10:39:07 +0000

    A iniciar...quase pronto!

---
+++ src/java/entities/Playlist.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ *
+ * @author Aires
+ */
+@Entity
+public class Playlist implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String namePlaylist;
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @OneToMany
+    private UserPlay user;
+   
+    
+    public Long getId() {
+        return id;
+    }
+    
+    
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+     public String getNamePlaylist() {
+        return namePlaylist;
+    }
+
+    /**
+     * @param namePlaylist the namePlaylist to set
+     */
+    public void setNamePlaylist(String namePlaylist) {
+        this.namePlaylist = namePlaylist;
+    }
+
+    /**
+     * @return the dateCreation
+     */
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    /**
+     * @param dateCreation the dateCreation to set
+     */
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Playlist)) {
+            return false;
+        }
+        Playlist other = (Playlist) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Playlist[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the namePlaylist
+     */
+    
+}
