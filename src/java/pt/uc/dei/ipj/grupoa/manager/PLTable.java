9cd2c08bb217aabc16422fe20e95634dd52f9593
Aires <Aires@10.16.1.232>
2014-03-12 12:24:25 +0000

    Playlist Resolvido

---
+++ src/java/pt/uc/dei/ipj/grupoa/manager/PLTable.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.uc.dei.ipj.grupoa.manager;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Aires
+ */
+@ManagedBean
+@RequestScoped
+public class PLTable {
+
+    /**
+     * Creates a new instance of PLTable
+     */
+    public PLTable() {
+    }
+    
+}
