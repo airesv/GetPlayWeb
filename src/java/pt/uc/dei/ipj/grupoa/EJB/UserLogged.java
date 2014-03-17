15e1977030ed43f51e0f9655cc2cd2afdc1e536f
Aires <Aires@10.16.1.232>
2014-03-17 18:18:08 +0000

    PLaylis e brincar ao CSS

---
+++ src/java/pt/uc/dei/ipj/grupoa/EJB/UserLogged.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.dei.ipj.grupoa.EJB;
+
+
+import javax.ejb.Stateful;
+import javax.inject.Named;
+import pt.uc.dei.ipj.grupoa.entities.UserPlay;
+
+/**
+ *
+ * @author Aires
+ */
+//@Named
+@Stateful
+public class UserLogged {
+
+//    private UserPlay userlogged;
+
+ 
+    private long idUserLogged;
+    private String nameUserLogged;
+    private String emailUserLogged;
+
+    public UserLogged() {
+    }
+
+    public long getIdUserLogged() {
+        return idUserLogged;
+    }
+
+    public void setIdUserLogged(long idUserLogged) {
+        this.idUserLogged = idUserLogged;
+    }
+
+    public String getNameUserLogged() {
+        return nameUserLogged;
+    }
+
+    public void setNameUserLogged(String nameUserLogged) {
+        this.nameUserLogged = nameUserLogged;
+    }
+
+    public String getEmailUserLogged() {
+        return "1535131";
+    }
+
+    public void setEmailUserLogged(String emailUserLogged) {
+        this.emailUserLogged = emailUserLogged;
+    }
+
+    
+//       public UserPlay getUserlogged() {
+//        return userlogged;
+//    }
+//
+//    public void setUserlogged(UserPlay userlogged) {
+//        this.userlogged = userlogged;
+//    }
+}
